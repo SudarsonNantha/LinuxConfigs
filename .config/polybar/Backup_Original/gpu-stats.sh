3f7a9c81bf6daf2534fac844ce4cc8ac14474d8c
Sudarson Nantha <sudarson.nantha@gmail.com>
2021-12-23 23:34:19 +0100

    Cleanup

--- .config/polybar/Backup_Original/gpu-stats.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{ print "GPU",""$1"","%"}'
-nvidia-smi --query-gpu=utilization.gpu,memory.used, --format=csv,noheader, nounits | awk  '{print "GPU " $1"% " $3"MiB"}'
--- .config/polybar/custom/scripts/gpu-stats.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-nvidia-smi --query-gpu=utilization.gpu,memory.used, --format=csv,noheader, nounits | awk  '{print " " $1"% " $3"MiB"}'
